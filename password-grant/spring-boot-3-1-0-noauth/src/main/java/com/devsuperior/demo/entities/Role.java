f2730ce2eb597cf8c84739e2c3beaba95b925ad5
matheus <129976192+matheustak@users.noreply.github.com>
2023-09-16 07:24:04 -0400

    userdetails
    not checkd list

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,14 +1,17 @@
 package com.devsuperior.demo.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name ="tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
