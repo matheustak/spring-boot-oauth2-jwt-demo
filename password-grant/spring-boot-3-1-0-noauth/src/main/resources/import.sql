76858a650464e1dfca72da60a577b01af00aec42
matheus <129976192+matheustak@users.noreply.github.com>
2023-09-15 21:38:10 -0400

    bcryptpassword
    gerando hash da senha com passwordEncoder

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$DO/cfDpIuJ0/PvfF5/02KeaZKHO4blXem3kuWKjVcSY1YOdOs7Uwy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$DO/cfDpIuJ0/PvfF5/02KeaZKHO4blXem3kuWKjVcSY1YOdOs7Uwy');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
